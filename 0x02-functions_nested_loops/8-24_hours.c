818b2c2b8a8f108bdb91e4b9ee91befc213af8dd
Iyamu Idahosa <worka3379@gmail.com>
2022-07-05 22:31:22 +0000

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,41 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
+* jack_bauer - prints every minute of the day
+* Return: returns nothing
+*/
 void jack_bauer(void)
 {
-	int i, j;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
 
-	i = 0;
-
-	while (i < 24)
+	while (1)
 	{
-		j = 0;
-		while (j < 60)
+		if (d > 9)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+			d = 0;
+			c++;
 		}
-		i++;
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		if (a == 2 && b > 3)
+			break;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+		d++;
 	}
 }
