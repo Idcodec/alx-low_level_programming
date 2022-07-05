92b71803207ae309bb5b640a0a2d572cd913e213
Iyamu Idahosa <worka3379@gmail.com>
2022-07-05 20:49:12 +0000

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet completely, 10 times
+* return: Returns nothing
+*/
+void print_alphabet_x10(void)
+{
+	int alpha;
+	int times10;
+
+	for (times10 = 0; times10 <= 10; times10++)
+{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+	_putchar(alpha);
+}
+	_putchar('\n');
+}
+}
