449a84621e9ddbd6918706c7fd77276c380d8f3a
Iyamu Idahosa <worka3379@gmail.com>
2022-07-05 20:52:14 +0000

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int alpha;
 	int times10;
 
-	for (times10 = 0; times10 <= 10; times10++)
+	for (times10 = 0; times10 <= 9; times10++)
 {
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
