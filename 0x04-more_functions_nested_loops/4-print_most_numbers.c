7a3167d761bf36af46d62c6f781bbb7117ea4019
Iyamu Idahosa <worka3379@gmail.com>
2022-07-07 21:29:57 +0000

    print numbers exce[t

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers 0 to 9, ten times
+* Return: returns nothing
+*/
+void more_numbers(void)
+{
+int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
