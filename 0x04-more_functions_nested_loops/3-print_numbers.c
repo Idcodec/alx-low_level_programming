12d3d63c1441c7ae217c8fe5371faf4ce8888b14
Iyamu Idahosa <worka3379@gmail.com>
2022-07-07 21:13:14 +0000

    Pint numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers 0 to 9
+* Return: returns nothing
+*/
+void print_numbers(void)
+{
+	int k;
+	for ( k = '0'; k <= '9'; k++)
+	putchar (k);
+	putchar ('\n');
+}
