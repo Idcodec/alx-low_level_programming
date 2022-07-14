e395c36525e8a0d70e802fb82e664a7fd6aef5bf
Iyamu Idahosa <worka3379@gmail.com>
2022-07-14 21:37:15 +0000

    print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,20 +10,17 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
-	}
-	else
+		_putchar('-');
+	} else
 	{
 		n1 = n;
 	}
 
-	if (n1 / 10 != 0)
-	{
+	if (n1 / 10)
 		print_number(n1 / 10);
-	}
+
 	_putchar((n1 % 10) + '0');
 }
