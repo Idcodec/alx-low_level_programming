a5e7a0738d60cf98a12e09556fcea3f4933499ad
Iyamu Idahosa <worka3379@gmail.com>
2022-07-18 19:32:55 +0000

    Copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@ include "main.h"
 * @n: Number of bytes to print
 * Return: dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
