03cd146104556cd63561b4bff06401d6f4f2c632
Iyamu Idahosa <worka3379@gmail.com>
2022-08-01 23:51:29 +0000

    A function to free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* free_dog - a function that fress dog
+* @d: struct to free
+* Return: return nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
