b22bb3b2d4609d236fe62f552a8a4bbbfb8138a7
Iyamu Idahosa <worka3379@gmail.com>
2022-08-01 21:11:10 +0000

    a program that prints the name of the file it was compiled from, followed by a new line

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
-main - prints the file it was compiled from
-Return: return 0
+* main - prints the file it was compiled from
+* Return: return 0
 */
 
 int main(void)
 {
-	printf ("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
