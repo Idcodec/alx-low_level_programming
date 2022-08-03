7e9662a691c75b6c982117ea21f314511f81a490
Iyamu Idahosa <worka3379@gmail.com>
2022-08-03 21:35:16 +0000

    a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,14 @@
 
 /**
 * print_name - a function that prints a name
-* @name: the name to print
-* @f: void function
-* parameter
-* Return: return nothing
+* @name: the name to be printed
+* @f: a pointer to a funtion that accepts a string as
+* parameter and returns nothing
+* Return: returns nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
