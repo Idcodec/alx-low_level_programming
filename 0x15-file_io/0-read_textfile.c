86adc2771b2a7726e6b81fbf96b1e339032b5af2
Idcode <Idahosabusiness@gmail.com>
2022-08-22 05:55:44 +0000

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,26 +8,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	ssize_t readlen, writelen;
 	char *buf;
-	ssize_t n, n1;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buf = malloc(sizeof(char) * letters);
-
-	n = read(fd, buf, letters);
-
-	n1 = write(STDOUT_FILENO, buf, n);
-	if (n != n1)
+	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
-
-	free(buf);
+	}
+	readlen = read(fd, buf, letters);
 	close(fd);
+	if (readlen == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	writelen = write(STDOUT_FILENO, buf, readlen);
+	free(buf);
+	if (readlen != writelen)
+		return (0);
+	return (writelen);
 
-	return (n);
 }
